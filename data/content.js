9f58f303d8945e230162360738def9d8c88d057a
Glade <gladekettle@gmail.com>
2019-07-17 18:45:07 +1000

    Fb and twitter adblock bug fix, Version update. Aria updates

--- data/content.js
+++ data/content.js
@@ -1,258 +1,266 @@
 let content = {
   props: {
-    title: 'COPIA | Metal band from Melbourne, Australia',
+    title: "COPIA | Metal band from Melbourne, Australia",
     meta: {
-      title: 'COPIA official band website | Metal band from Melbourne, Australia',
-      description: '',
-      keywords: 'copia, heavy metal, band, melbourne band, progressive rock, metalcore, alternative metal, german heavy metal, hard rock, metal band, epic rock, epic metal, australian heavy metal, CopiaNewAlbum ,Chris Blancato Audio ,Will Putney ,PRS ,KSD ,Warwick ,Mayones ,Schecter ,Ibanez ,Guitar ,Bass ,Drums ,ProTools ,DarkGlass ,BKP ,BareKnuckle ,Kemper ,AxeFx ,Orange ,TubeScreamer ,Peavy ,Amplifier ,Djent',
-      image: 'http://copiaband.com/content/images/og-share-1200x630.jpg',
-      siteUrl: 'http://copiaband.com/'
+      title:
+        "COPIA official band website | Metal band from Melbourne, Australia",
+      description: "",
+      keywords:
+        "copia, heavy metal, band, melbourne band, progressive rock, metalcore, alternative metal, german heavy metal, hard rock, metal band, epic rock, epic metal, australian heavy metal, CopiaNewAlbum ,Chris Blancato Audio ,Will Putney ,PRS ,KSD ,Warwick ,Mayones ,Schecter ,Ibanez ,Guitar ,Bass ,Drums ,ProTools ,DarkGlass ,BKP ,BareKnuckle ,Kemper ,AxeFx ,Orange ,TubeScreamer ,Peavy ,Amplifier ,Djent",
+      image: "http://copiaband.com/content/images/og-share-1200x630.jpg",
+      siteUrl: "http://copiaband.com/"
     },
     externalLinks: [
       {
         attributes: {
-          href: 'https://fonts.googleapis.com/css?family=Open+Sans|Sumana',
-          rel: 'stylesheet'
+          href: "https://fonts.googleapis.com/css?family=Open+Sans|Sumana",
+          rel: "stylesheet"
         }
       }
     ],
     navigation: {
       links: [
         {
-          text: 'Home',
-          url: '/#/home',
+          text: "Home",
+          url: "/#/home",
           attributes: false
         },
         {
-          text: 'Tour',
-          url: '#/tour',
+          text: "Tour",
+          url: "#/tour",
           attributes: false
         },
         {
-          text: 'Shop',
-          url: 'http://www.copia.bigcartel.com/',
+          text: "Shop",
+          url: "http://www.copia.bigcartel.com/",
           attributes: {
-            target: '_blank'
+            target: "_blank"
           }
         },
         {
-          text: 'Subscribe',
-          url: 'http://eepurl.com/CC0sz',
+          text: "Subscribe",
+          url: "http://eepurl.com/CC0sz",
           attributes: {
-            target: '_blank'
+            target: "_blank"
           }
         },
         {
-          text: 'Contact',
-          url: 'mailto:copiaband@gmail.com',
+          text: "Contact",
+          url: "mailto:copiaband@gmail.com",
           attributes: {
-            target: '_blank'
+            target: "_blank"
           }
         }
       ]
     },
     navigation_v2: {
+      menu_icon: {
+        openUrl: "/content/symbols.svg?v=1#menu-icon",
+        closeUrl: "/content/symbols.svg?v=1#close-icon"
+      },
       launcher_links: [
         {
-          text: 'Menu',
-          url: '#menu',
+          text: "Menu",
+          url: "#menu",
           attributes: false
         }
       ],
       social_links: [
         {
-          text: 'Facebook',
-          url: 'https://www.facebook.com/copiaband/',
+          text: "Facebook",
+          url: "https://www.facebook.com/copiaband/",
           attributes: {
-            target: '_blank'
+            target: "_blank"
           },
           icon: {
-            url: '/content/symbols.svg#facebook-icon'
+            url: "/content/symbols.svg?v=1#fbz-icon"
           }
         },
         {
-          text: 'Youtube',
-          url: 'https://www.youtube.com/user/copiaband',
+          text: "Youtube",
+          url: "https://www.youtube.com/user/copiaband",
           attributes: {
-            target: '_blank'
+            target: "_blank"
           },
           icon: {
-            url: '/content/symbols.svg#youtube-icon'
+            url: "/content/symbols.svg?v=1#youtube-icon"
           }
         },
         {
-          text: 'Instagram',
-          url: 'https://www.instagram.com/copiaband/',
+          text: "Instagram",
+          url: "https://www.instagram.com/copiaband/",
           attributes: {
-            target: '_blank'
+            target: "_blank"
           },
           icon: {
-            url: '/content/symbols.svg#instagram-icon'
+            url: "/content/symbols.svg?v=1#instagram-icon"
           }
         },
         {
-          text: 'Twitter',
-          url: 'https://twitter.com/copiaband',
+          text: "Twitter",
+          url: "https://twitter.com/copiaband",
           attributes: {
-            target: '_blank'
+            target: "_blank"
           },
           icon: {
-            url: '/content/symbols.svg#twitter-icon'
+            url: "/content/symbols.svg?v=1#twt-icon"
           }
         }
       ],
       menu_links: [
         {
-          text: 'Home',
-          url: '/#/home',
+          text: "Home",
+          url: "/#/home",
           attributes: {
-            style: 'animation-delay: 0s'
+            style: "animation-delay: 0s"
           }
         },
         {
-          text: 'Tour',
-          url: '#/tour',
+          text: "Tour",
+          url: "#/tour",
           attributes: {
-            style: 'animation-delay: .0625s'
+            style: "animation-delay: .0625s"
           }
         },
         {
-          text: 'Shop',
-          url: 'http://www.copia.bigcartel.com/',
+          text: "Shop",
+          url: "http://www.copia.bigcartel.com/",
           attributes: {
-            target: '_blank',
-            style: 'animation-delay: 0.125s'
+            target: "_blank",
+            style: "animation-delay: 0.125s"
           }
         },
         {
-          text: 'Subscribe',
-          url: 'http://eepurl.com/CC0sz',
+          text: "Subscribe",
+          url: "http://eepurl.com/CC0sz",
           attributes: {
-            target: '_blank',
-            style: 'animation-delay: .1875s'
+            target: "_blank",
+            style: "animation-delay: .1875s"
           }
         },
         {
-          text: 'Contact',
-          url: 'mailto:copiaband@gmail.com',
+          text: "Contact",
+          url: "mailto:copiaband@gmail.com",
           attributes: {
-            target: '_blank',
-            style: 'animation-delay: .25s'
+            target: "_blank",
+            style: "animation-delay: .25s"
           }
         }
-        ]
+      ]
     },
     social: {
       links: [
         {
-          text: 'Facebook',
-          url: 'https://www.facebook.com/copiaband/',
+          text: "Facebook",
+          url: "https://www.facebook.com/copiaband/",
           attributes: {
-            target: '_blank'
+            target: "_blank"
           },
           icon: {
-            url: '/content/symbols.svg#facebook-icon'
+            url: "/content/symbols.svg?v=2#fbz-icon"
           }
         },
         {
-          text: 'Youtube',
-          url: 'https://www.youtube.com/user/copiaband',
+          text: "Youtube",
+          url: "https://www.youtube.com/user/copiaband",
           attributes: {
-            target: '_blank'
+            target: "_blank"
           },
           icon: {
-            url: '/content/symbols.svg#youtube-icon'
+            url: "/content/symbols.svg?v=1#youtube-icon"
           }
         },
         {
-          text: 'Instagram',
-          url: 'https://www.instagram.com/copiaband/',
+          text: "Instagram",
+          url: "https://www.instagram.com/copiaband/",
           attributes: {
-            target: '_blank'
+            target: "_blank"
           },
           icon: {
-            url: '/content/symbols.svg#instagram-icon'
+            url: "/content/symbols.svg?v=1#instagram-icon"
           }
         },
         {
-          text: 'Twitter',
-          url: 'https://twitter.com/copiaband',
+          text: "Twitter",
+          url: "https://twitter.com/copiaband",
           attributes: {
-            target: '_blank'
+            target: "_blank"
           },
           icon: {
-            url: '/content/symbols.svg#twitter-icon'
+            url: "/content/symbols.svg?v=2#twt-icon"
           }
         }
       ]
     },
     home: {
-      title: 'COPIA - Heavy metal band from Melbourne, Australia',
+      title: "COPIA - Heavy metal band from Melbourne, Australia",
       logo: {
-        url: '/content/symbols.svg#icon-logo'
+        url: "/content/symbols.svg?v=1#icon-logo"
       },
       hero: {
-        url: '/content/logo.svg',
-        bg: '/content/hero-bg.jpg'
+        url: "/content/logo.svg",
+        bg: "/content/hero-bg.jpg"
       },
       featuredAlbum: {
-        url: '',
-        text: 'ALBUM - Epoch',
+        url: "",
+        text: "ALBUM - Epoch",
         btn: [
           {
-            'text': 'Buy Physical',
-            'url': 'http://copia.bigcartel.com/product/epoch-album'
+            text: "Buy Physical",
+            url: "http://copia.bigcartel.com/product/epoch-album"
           },
           {
-            'text': 'Buy Digital',
-            'url': 'https://itunes.apple.com/au/album/epoch/id1206217201?ls=1&app=itunes'
+            text: "Buy Digital",
+            url:
+              "https://itunes.apple.com/au/album/epoch/id1206217201?ls=1&app=itunes"
           }
         ],
         img: {
-          alt: 'COPIA - Epoch',
-          url: '/content/images/copia-epoch-400x400.jpg'
+          alt: "COPIA - Epoch",
+          url: "/content/images/copia-epoch-400x400.jpg"
         }
       },
-      bg: '/content/images/bg-image.jpg'
+      bg: "/content/images/bg-image.jpg"
     },
     sms: {
-      'title': 'Want to hear when Copia are performing near you?',
-      'body': 'Text your closest city (or cities) name to your country\'s number and receive tour dates directly to your phone!',
-      'numbers': [
+      title: "Want to hear when Copia are performing near you?",
+      body:
+        "Text your closest city (or cities) name to your country's number and receive tour dates directly to your phone!",
+      numbers: [
         {
-          'title': 'DE',
-          'phone': '+49177 17816878'
+          title: "DE",
+          phone: "+49177 17816878"
         },
         {
-          'title': 'NL',
-          'phone': '+3197004499559'
+          title: "NL",
+          phone: "+3197004499559"
         },
         {
-          'title': 'CH',
-          'phone': '+41766013374'
+          title: "CH",
+          phone: "+41766013374"
         },
         {
-          'title': 'BE',
-          'phone': '+32460210368'
+          title: "BE",
+          phone: "+32460210368"
         },
         {
-          'title': 'AUT',
-          'phone': '+43676800800707'
+          title: "AUT",
+          phone: "+43676800800707"
         }
       ]
     },
     video: {
-      title: 'Video',
-      src: 'https://www.youtube.com/embed/n1qw9gM9V9c?rel=0'
+      title: "Video",
+      src: "https://www.youtube.com/embed/n1qw9gM9V9c?rel=0"
     },
     tour: {
-      title: 'Tour',
-      logoUrl: '/content/symbols.svg#icon-logo'
+      title: "Tour",
+      logoUrl: "/content/symbols.svg?v=1#icon-logo"
     },
     footer: {
-      text: '&copy; COPIA 2017. All rights reserved.'
+      text: "&copy; COPIA 2017. All rights reserved."
     }
   }
-}
+};
 
-module.exports = content
+module.exports = content;
